65f58262c30893f4f3f368f1580cc664fe4279cf
kallard <kallard@student.21-school.ru>
2021-06-04 11:30:22 +0300

    main.cpp contains now server logic

--- irc_cpp/sources/main.cpp
+++ irc_cpp/sources/main.cpp
@@ -1,83 +1,117 @@
 #include <iostream>
 #include <fcntl.h>
 #include <sys/socket.h> 
-#include <netinet/in.h> //для sockaddr_in - используется для хранения адресов ipv4
+#include <netinet/in.h> //для sockaddr_in
 #include <unistd.h> 
 #include <cstdlib> // for atoi
+#include <set> // for std::set
+#include <algorithm> // for std::max
 
 int main(int argc, char **argv) {
-
-	if(argc != 3) {
-		std::cout << "Usage ./ircserv <port> <password>\n";
-		return 0; 
+	if (argc != 3) {
+		std::cout << "Error! Usage ./ircserv <port> <password>\n";
+		exit(1); 
 	}
-	int PORT = std::atoi(argv[1]);
-	std::string PASS(argv[2]);
 
+	int					PORT			= std::atoi(argv[1]);
+	std::string			PASS(argv[2]);
+	int 				server_fd		= 0;
+	struct sockaddr_in	address;		// для хранения адресов ipv4
+	socklen_t			size			= sizeof(address);
+	char				buf[1024]		= {0}; // буфер для чтения из сокетов
+    int					bytes_read		= 0;
 
-	int server_fd;
 	// socket - Создать новый поточный Интернет-сокет (TCP/IP) и вернуть его файловый дескриптор
 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
-		std::cout << "Error creating socket\n"; 
-		return 0; 
+		perror("socket");
+        exit(2);
 	}
-
 	fcntl(server_fd, F_SETFL, O_NONBLOCK);  // устанавливается неблокирующий доступ к фд
 
-	struct sockaddr_in address;
-
+	//Инициализация структуры с адресом
 	memset((char *)&address, 0, sizeof(address)); 
 	address.sin_family = AF_INET; 
 	address.sin_addr.s_addr = htonl(INADDR_ANY);
 	address.sin_port = htons(PORT);
+
 	// bind - Связать сокет с IP-адресом и портом
 	if (bind(server_fd,(struct sockaddr *)&address,sizeof(address)) < 0) { 
-		std::cout << "Bind failed\n"; 
-		return 0; 
+		perror("bind");
+        exit(3);
 	}
 
 	//listen - Объявить о желании принимать соединения. Слушает порт и ждет когда будет установлено соединение
 	if (listen(server_fd, 10) < 0) { // 10 - максимальное количество соединений, которые могут быть поставлены в очередь
-		std::cout << "Listen error\n"; 
-		return 0; 
+		perror("listen");
+        exit(4);
 	}
+
+	//сет с клиентами, подключенными к нашему серверу
+	std::set<int> clients_fd;
+    clients_fd.clear();
+
 	int new_socket = 0;
-	socklen_t size = sizeof(address);
 
 	while(1) {
-		std::cout << "\n+++++++ Waiting for new connection ++++++++\n\n";
-		// accept - Принять запрос на установку соединения
-		if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &size)) < 0) {
-			std::cout << "Accept error\n"; 
-			return 0; 
+		// Заполняем сет сокетов на мониторинг готовности к чтению
+		fd_set readset;
+		FD_ZERO(&readset);
+		FD_SET(server_fd, &readset);
+		for(std::set<int>::iterator it = clients_fd.begin(); it != clients_fd.end(); it++)
+            FD_SET(*it, &readset);
+
+		// Заполняем сет сокетов на мониторинг готовности к записи
+		fd_set writeset;
+		FD_ZERO(&writeset);
+		FD_SET(server_fd, &writeset);
+		for(std::set<int>::iterator it = clients_fd.begin(); it != clients_fd.end(); it++)
+            FD_SET(*it, &writeset);
+
+		// mx+1 - число, на единицу большее максимального файлового дескриптора в любом из наборов (1 аргумент selecta)
+		int mx = std::max(server_fd, *max_element(clients_fd.begin(), clients_fd.end()));
+
+		std::cout << "\n+++++++ Waiting ++++++++\n\n";
+		// select - Ждём события в одном из сокетов
+		int total_fds = 0; 		// total_fds - суммарное кол-во фд, сработавших в селекте
+		if ((total_fds = select(mx + 1, &readset, &writeset, NULL, NULL)) == -1) {
+    		perror("select");
+            exit(5);
 		}
 
-	// fd_set fd_set_read;
-	// fd_set fd_set_write;
-	// FD_ZERO(&fd_set_read);
-	// FD_ZERO(&fd_set_write);
+		// FD_ISSET - Определяем тип события и выполняем соответствующие действия:
+		if (FD_ISSET(server_fd, &readset))
+        {
+			int new_socket = 0;
+            // Поступил новый запрос на соединение, принимаем с помощью accept
+            if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &size)) < 0) {
+                perror("accept");
+                exit(6);
+            }
+            fcntl(new_socket, F_SETFL, O_NONBLOCK);
+            clients_fd.insert(new_socket);
+			std::cout << "\n+++++++ New client joined! ++++++++\n\n";
+        }
 
-/*
-
-
-*/
-
-
-
-
-		char buffer[1024] = {0};
-
-	//  recv - получение/чтение данных из сокета
-		int valread = recv(new_socket, buffer, 1024, 0);
-		std::cout << buffer;
-		if (valread < 0) { 
-			printf("No bytes are there to read");
-		}
-		std::string hello = "Hello from the server";
-	//  send - отправка данных
-		send(new_socket, hello.c_str(), hello.length(), 0);
+		for (std::set<int>::iterator it = clients_fd.begin(); it != clients_fd.end(); it++)
+        {
+            if (FD_ISSET(*it, &readset)) {
+                // Поступили данные от клиента, читаем их
+                if ((bytes_read = recv(*it, buf, 1024, 0)) <= 0) {
+                    // Соединение разорвано, удаляем сокет из сета
+                    close(*it);
+                    clients_fd.erase(*it);
+                    continue;
+				}
+                // Отправим данные от клиента парсеру
+                // parser(*it, buf, bytes_read, 0); // (фд клиента, буфер с сообщением, размер сообщения)
+            }
+            if (FD_ISSET(*it, &writeset)) {
+				// Посмотрим буфер этого клиента, если есть, что ему писать, то сливаем ему буфер, буфер очищаем
+				// std::string client_buf;
+				// send(*it, client_buf.c_str(), client_buf.length(), 0);
+			}
+        }
 		
-		close(new_socket);
 		return 0;
 	}
 }
