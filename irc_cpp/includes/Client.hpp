30b24f4f869b09b6d2eed9230bcd2335f199477b
Marine Maida <mmaida@il-h1.msk.21-school.ru>
2021-06-04 21:46:29 +0300

    Client class not done

--- irc_cpp/includes/Client.hpp
+++ irc_cpp/includes/Client.hpp
@@ -6,8 +6,33 @@ class Client
 private:
 	int					_fd;
 	struct sockaddr_in	_address;
+	std::string			_nick;
+	std::string			_user;
+	std::string			_host;
+	std::string			_server;
+	std::string			_real;
+	bool				_isreg; // зарегистрирован ли клиент (только после NICK USER)
 
 public:
 	Client(int fd, struct sockaddr_in address);
+	Client(const Client& src);
+	Client& operator=(const Client& src);
 	~Client();
+
+	// Set nick (для NICK)
+	void		setNick(std::string nick) {this->_nick = nick;};
+
+	// Set userdata и помечает клиета как зарегестрированный (для USER)
+	void		setUserData(const std::string &user, const std::string &host, 
+							const std::string &server, const std::string &real);
+
+	// Зарегестрированн ли клиент (незарегестрированному клиенту недоступны никакие команды кроме PASS USER NICK)
+	bool		isReg() {return this->_isreg;};
+
+	// Getters
+	const std::string	&getNick() {return this->_nick;};
+	const std::string	&getUser() {return this->_user;};
+	const std::string	&getHost() {return this->_host;};
+	const std::string	&getServer() {return this->_server;};
+	const std::string	&getReal() {return this->_real;};
 };
--- irc_cpp/sources/Client.cpp
+++ irc_cpp/sources/Client.cpp
@@ -2,8 +2,34 @@
 
 Client::Client(int fd, struct sockaddr_in address) : _fd(fd), _address(address)
 {
+	this->_isreg = false;
+}
+
+Client::Client(const Client &src)
+{
+	*this = src;
+}
+
+Client &Client::operator=(const Client &src)
+{
+	_user = src._user;
+	_host = src._host;
+	_server = src._server;
+	_real = src._real;
+	_isreg = src._isreg; 
+	return *this;
 }
 
 Client::~Client()
 {
+}
+
+void	Client::setUserData(const std::string &user, const std::string &host, 
+							const std::string &server, const std::string &real)
+{
+	this->_user = user;
+	this->_host = host;
+	this->_server = server;
+	this->_real = real;
+	this->_isreg = true; 
 }
