e5fa91ec891916d0f9c8ae3d46410d920e38b963
k-allard <kallard@student.21-school.ru>
2021-06-22 13:34:19 +0300

    fixed segv in checkfds(), removed some obvious comments

--- irc_cpp/sources/Server.cpp
+++ irc_cpp/sources/Server.cpp
@@ -11,7 +11,7 @@ Server::Server(int port, std::string pass) : _port(port), _pass(pass), _server_f
 	_clients_fd.clear();
 	_clients.clear();
 	_parser = new Parser(*this);
-};
+}
 
 Server::~Server() {
 	std::map<int, Client*>::iterator it;
@@ -39,14 +39,12 @@ void Server::initFds() {
 	FD_SET(_server_fd, &_writeset);
 	for (std::set<int>::iterator it = _clients_fd.begin(); it != _clients_fd.end(); it++)
 		FD_SET(*it, &_writeset);
-	_mx = 0;
 	if (!_clients_fd.empty())
 		_mx = std::max(_server_fd, *_clients_fd.rbegin());
 	else
 		_mx = _server_fd;
 }
 
-// select - Ждём события в одном из сокетов
 void Server::doSelect() {
 	int total_fds = 0;
 	if ((total_fds = select(_mx + 1, &_readset, &_writeset, NULL, NULL)) == -1) {
@@ -72,7 +70,6 @@ std::cout << "Our clients: ";
 std::string Server::getNamesNotInChannels()
 {
 	std::string names = "";
-
 	for (std::map<int,Client *>::iterator it=_clients.begin(); it!=_clients.end(); ++it) {
 		int isFound = 0;
 		for (std::map<std::string, Channel *>::iterator it_ch=_channels.begin(); it_ch!=_channels.end() && isFound == 0; ++it_ch) {
@@ -90,7 +87,6 @@ std::string Server::getNamesNotInChannels()
 }
 
 void Server::processMessage(const Message *msg, int fd, Client *client, Cmds *cmds) {
-
     switch (msg->command->cmdType) {
 		case MsgCmd__EMPTY :
 			break;
@@ -177,42 +173,33 @@ void Server::processMessage(const Message *msg, int fd, Client *client, Cmds *cm
 
 void Server::checkFds() {
 	int					bytes_read		= 0;
-	struct sockaddr_in	address;		// структура для хранения адресов ipv4
-    Cmds cmds(*this);
+	int					new_socket		= 0;
+	struct sockaddr_in	address;
+    Cmds				cmds(*this);
 
 	bzero(_buf, 512);
-
-	int new_socket = 0;
-	// FD_ISSET - Определяем тип события и выполняем соответствующие действия:
 	if (FD_ISSET(_server_fd, &_readset)) {
 		int			new_socket	= 0;
 		socklen_t	size		= sizeof(address);
-		// Поступил новый запрос на соединение, принимаем с помощью accept
 		if ((new_socket = accept(_server_fd, (struct sockaddr *)&address, &size)) < 0) {
 			std::cerr << "Error in accept: " << strerror(errno) << std::endl;
 			exit(6);
 		}
 		fcntl(new_socket, F_SETFL, O_NONBLOCK);
-		// Добавляем подключившегося клиента в список наших клиентов
 		_clients_fd.insert(new_socket);
-		// Создаем новый инстанс класса клиент, передаем ему его фд и адрес
 		Client *client = new Client(new_socket, address);
-		// добавить его в список (мапу) наших клиентов, поставив фд в каестве ключа
 		_clients.insert(std::pair< int,Client* >(new_socket, client));
 		std::cout << "[ircserv.net] New client joined!" << std::endl;
-//		printClients();
 	}
 	for (std::set<int>::iterator it = _clients_fd.begin(); it != _clients_fd.end(); it++) {
+		int temp_fd = *it;
 		if (FD_ISSET(*it, &_readset)) {
-			// Поступили данные от клиента, читаем их
 			if ((bytes_read = recv(*it, _buf, 512, 0)) <= 0) {
-				// Соединение разорвано, удаляем сокет из сета
 				cmds.QUITCmd(*it);
 				break ;
 			}
-			//std::cout <<  _buf <<std::endl;
 			_clients.at(*it)->appendMessageBuffer(_buf);
-			if(_clients.at(*it)->isMessageBufferComplete())
+			if (_clients.at(*it)->isMessageBufferComplete())
 			{
 				std::vector<Message> msgs = _parser->do_parsing(
 						*it,
@@ -222,19 +209,14 @@ void Server::checkFds() {
 				{
                     this->processMessage(&(*msg), *it, _clients.at(*it), &cmds);
 				}
-				if(_clients.find(*it) != _clients.end())
-				    _clients.at(*it)->clearMessageBuffer();
+				if (_clients.find(temp_fd) != _clients.end())
+				    _clients.at(temp_fd)->clearMessageBuffer();
                 else
-                {
-//                    _clients_fd.erase(*it);
                     break;
-                }
 			}
 		}
 		if (FD_ISSET(*it, &_writeset)) {
-			// Посмотрим буфер этого клиента, если есть, что ему писать, то отправим это ему, буфер очистим
 			if (!_clients.at(*it)->_buf.empty()) {
-//			    std::cout << "Rpl: " << _clients.at(*it)->_buf.front();
 				send(*it, _clients.at(*it)->_buf.front().c_str(), _clients.at(*it)->_buf.front().size(), 0);
 				_clients.at(*it)->_buf.front().erase();
 				_clients.at(*it)->_buf.pop();
@@ -257,22 +239,18 @@ void Server::initServer() {
 		std::cerr << "Error in socket: " << strerror(errno) << std::endl;
 		exit(2);
 	}
-	fcntl(_server_fd, F_SETFL, O_NONBLOCK);  // устанавливается неблокирующий доступ к фд
-
-	//Инициализация структуры с адресом
+	fcntl(_server_fd, F_SETFL, O_NONBLOCK);
 	memset((char *)&_address, 0, sizeof(_address));
 	_address.sin_family = AF_INET;
 	_address.sin_addr.s_addr = htonl(INADDR_ANY);
 	_address.sin_port = htons(_port);
 
-	// bind - Связать сокет с IP-адресом и портом
 	if (bind(_server_fd,(struct sockaddr *)&_address,sizeof(_address)) < 0) {
 		std::cerr << "Error in bind: " << strerror(errno) << std::endl;
 		exit(3);
 	}
 
-	//listen - Объявить о желании принимать соединения. Слушает порт и ждет когда будет установлено соединение
-	if (listen(_server_fd, 10) < 0) { // 10 - максимальное количество соединений, которые могут быть поставлены в очередь
+	if (listen(_server_fd, 10) < 0) {
 		std::cerr << "Error in listen: " << strerror(errno) << std::endl;
 		exit(4);
 	}
